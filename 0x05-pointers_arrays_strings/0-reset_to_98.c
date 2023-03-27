d79874d3cf04b66e9a4d917e2a79bf80433b8ee3
Stosh09 <stoshochieng@gmail.com>
2023-03-28 01:43:39 +0300

    Pointers Assignment 1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,11 @@
-/*
- * File: 0-reset_to_98.c
- * Auth: Brennan D Baraban
- */
+
 
 #include "main.h"
 
 /**
- * reset_to_98 - Takes a pointer to an int and updates
- *               the value it points to to 98.
- * @n: The pointer to an int.
+ * reset_to_98 - takes a pointer to an int as parameter
+ * and updates the value to 98
+ * @n: parameter being updated
  */
 void reset_to_98(int *n)
 {
