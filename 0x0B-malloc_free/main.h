1fad92ae149c223c0f882e3b76070c1113c1a021
Stosh09 <stoshochieng@gmail.com>
2023-04-24 14:28:32 +0300

    Create array dynamically

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,20 +1,12 @@
 
-#define MAIN_H
-
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
 int _putchar(char c);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-
-
-
-
-
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+#endif
