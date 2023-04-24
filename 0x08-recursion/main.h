c2e37c0399ff7eb233c690608f63fe69139c7147
Stosh09 <stoshochieng@gmail.com>
2023-04-24 12:22:12 +0300

    Prints string followed by new line

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,22 +1,10 @@
-ifndef MAIN_H
-#define MAIN_H
-
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
 int _putchar(char c);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-
-
-
-
-
-
-#endif /* MAIN_H */
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
