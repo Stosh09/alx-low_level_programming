44d7bca441a46369da09c161036f47854d192a53
Stosh09 <stoshochieng@gmail.com>
2023-06-07 22:13:15 +0300

    Palindrome fix

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,14 +1,13 @@
 #include "stdio.h"
 #include "main.h"
-
 /**
- * palind2 - obtains length of a
- * @a: string
- * @l: integer to count length
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+* palind2 - obtains length of a
+* @a: string
+* @l: integer to count length
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
 int palind2(char *a, int l)
 {
 	
