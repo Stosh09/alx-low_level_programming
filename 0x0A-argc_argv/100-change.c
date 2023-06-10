0da255a3c89b4ce90b80ae7891c00d3cdf6130fe
Stosh09 <stoshochieng@gmail.com>
2023-06-10 19:07:16 +0300

    Formatting errors

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,74 +12,73 @@
 int main(int argc, char *argv[])
 {
 
-    int cents, coins = 0;
+int cents, coins = 0;
 
-    if (argc == 2)
-    {
+if (argc == 2)
+{
 
-        cents = atoi(*(argv + 1));
+cents = atoi(*(argv + 1));
 
-        while (cents > 0)
+while (cents > 0)
 
-        {
+{
 
-            if (cents % 25 < cents)
+if (cents % 25 < cents)
 
-            {
+{
 
-                cents -= 25;
+cents -= 25;
 
-                coins++;
-            }
+coins++;
+}
 
-            else if (cents % 10 < cents)
+else if (cents % 10 < cents)
 
-            {
+{
 
-                cents -= 10;
+cents -= 10;
 
-                coins++;
-            }
+coins++;
+}
+else if (cents % 5 < cents)
 
-            else if (cents % 5 < cents)
+{
 
-            {
+cents -= 5;
 
-                cents -= 5;
+coins++;
+}
 
-                coins++;
-            }
+else if (cents % 2 < cents)
 
-            else if (cents % 2 < cents)
+{
 
-            {
+cents -= 2;
 
-                cents -= 2;
+coins++;
+}
 
-                coins++;
-            }
+else if (cents % 1 < cents)
 
-            else if (cents % 1 < cents)
+{
 
-            {
+cents -= 1;
 
-                cents -= 1;
+coins++;
+}
+}
+}
 
-                coins++;
-            }
-        }
+else
+
+{
+
+printf("Error\n");
+
+return (1);
     }
 
-    else
+printf("%d\n", coins);
 
-    {
-
-        printf("Error\n");
-
-        return (1);
-    }
-
-    printf("%d\n", coins);
-
-    return (0);
+return (0);
 }
