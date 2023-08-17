672b7576f5cb335f479f78f68bda5bc70fa74da2
Stosh09 <stoshochieng@gmail.com>
2023-08-17 23:48:10 +0300

    Get sum of value in the list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,20 +1,39 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - Sums all the data of a dlistint_t list.
- * @head: The head of the dlistint_t list.
- *
- * Return: The sum of all the data.
+ * sum_dlistint - returns sum of all data(n) of a dlistint_t list
+ * @head: pointer to the first node of the list
+ * Return: sum of data(n) or 0
  */
 int sum_dlistint(dlistint_t *head)
 {
+	dlistint_t *iter = NULL;
 	int sum = 0;
+	size_t forward = 1;
 
-	while (head)
+	if (head == NULL)
+		return (0);
+	if (head->next && head->prev)
 	{
-		sum += head->n;
-		head = head->next;
+		iter = head->next;
+		while (head)
+		{
+			sum += head->n;
+			head = head->prev;
+		}
+		while (iter)
+		{
+			sum += iter->n;
+			iter = iter->next;
+		}
+		return (sum);
 	}
-
+	iter = head;
+	forward = iter->next ? 1 : 0;
+	while (iter)
+	{
+		sum += iter->n;
+		iter = forward ? iter->next : iter->prev;
+	}
 	return (sum);
 }
