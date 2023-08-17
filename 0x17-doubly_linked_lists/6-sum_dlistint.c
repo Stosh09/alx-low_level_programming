915e811986481bcb92c9716ed94bd6b7c905bacf
Stosh09 <stoshochieng@gmail.com>
2023-08-17 23:42:35 +0300

    Get sum of value in the list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - Sum all the data of a list
+ * @head: Head of the list
+ *
+ * Return the sum of all the data
+*/
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (sum)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
