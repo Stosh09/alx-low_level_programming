2314456789c1e8bebde474389bc8670101306c35
Stosh09 <stoshochieng@gmail.com>
2023-08-22 02:04:11 +0300

    A script that makes us winners

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Stosh09/alx-low_level_programming/master/0x18-dynamic_libraries/
-export LD_PRELOAD=./libwin.so
+wget -P /tmp/ https://raw.github.com/Stosh09/alx-low_level_programming/master/0x18-dynamic_libraries/randwin.so
+export LD_PRELOAD=./randwin.so
