ed101e2296966d7c0f9b9268570f435adaf78205
Stosh09 <stoshochieng@gmail.com>
2023-12-13 23:20:08 +0300

    function that searches for a value in a sorted array of integers using the Interpolation search algorithm

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -12,8 +12,7 @@ include "search_algos.h"
 	 *
 	 * Description: Prints a value every time it is compared in the array..
 	 */
-	int
-	interpolation_search(int *array, size_t size, int value)
+int interpolation_search(int *array, size_t size, int value)
 {
 	size_t i, l, r;
 
